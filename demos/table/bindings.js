73bafee471759a2a49c9b2d7cdf10ccb68cea150
Izaak Schroeder <izaakschroeder@aelatis.com>
2012-02-17 20:48:53 -0800

    Updated bindings to match template render stack system.

--- demos/table/bindings.js
+++ demos/table/bindings.js
@@ -5,13 +5,14 @@
 			".Name": "name",
 			".Age": "age",
 			"[name=fruit] option": {
-				data: function(data, context) {
-					return context.data.fruits; 
+				data: function(data, context, sys) {
+					return sys.stack[0].data.fruits;
 				},
 				attributes: {
 					value: "id",
-					selected: function(user) {
-						return user.fruit === this.value ? "selected" : undefined;
+					selected: function(data, context, sys) {
+						var fruit = sys.stack[sys.stack.length - 3].data.fruit;
+						return data.id === fruit ? "selected" : undefined;
 					}
 				},
 				content: "name"
